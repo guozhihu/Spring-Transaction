540e5e728ba968bc7a9be7d15c3bbc5d23e5380c
guozhihao <283839751@qq.com>
2019-03-27 19:31:23 +0800

    链式事务-DatasourceTransactionManager（DB-DB）

---
+++ tran-5-spring-dtx-db-db/src/main/java/com/spring/dtx/springdtxdbdb/Tran5SpringDtxDbDbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dtx.springdtxdbdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tran5SpringDtxDbDbApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Tran5SpringDtxDbDbApplication.class, args);
+    }
+    
+}
