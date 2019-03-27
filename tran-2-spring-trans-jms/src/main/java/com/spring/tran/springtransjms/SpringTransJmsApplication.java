78d8fedc6ab9531d19fc66c28886119a01704408
guozhihao <283839751@qq.com>
2019-03-27 19:27:22 +0800

    Jms-session事务实例和Jms-spring事务实例，JmsTransactionManager

---
+++ tran-2-spring-trans-jms/src/main/java/com/spring/tran/springtransjms/SpringTransJmsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tran.springtransjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransJmsApplication.class, args);
+    }
+
+}
