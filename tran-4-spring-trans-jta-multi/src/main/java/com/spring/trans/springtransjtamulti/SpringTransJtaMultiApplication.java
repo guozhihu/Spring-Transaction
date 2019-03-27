bd56c81ed3dbe2fc02643ddb7ce42aef760306e5
guozhihao <283839751@qq.com>
2019-03-27 19:28:45 +0800

    JTA事务实例---多数据源

---
+++ tran-4-spring-trans-jta-multi/src/main/java/com/spring/trans/springtransjtamulti/SpringTransJtaMultiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.trans.springtransjtamulti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransJtaMultiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransJtaMultiApplication.class, args);
+    }
+
+}
