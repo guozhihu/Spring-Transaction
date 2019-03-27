7ba29f1771f7cce744d636b64f06f59e8433fb34
guozhihao <283839751@qq.com>
2019-03-27 19:32:47 +0800

    链式事务-JpaTransactionManager（DB-DB）

---
+++ tran-6-spring-dtx-jpa-db/src/main/java/com/spring/dtx/tran6springdtxjpadb/Tran6SpringDtxJpaDbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dtx.tran6springdtxjpadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tran6SpringDtxJpaDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tran6SpringDtxJpaDbApplication.class, args);
+    }
+
+}
