a072b427a0b173ef46e7fe72b7ed763dd4eeeb94
guozhihao <283839751@qq.com>
2019-03-27 19:33:58 +0800

    最大努力一次提交：TransactionAwareConnectionFactoryProxy（JMS-DB）

---
+++ tran-7-spring-dtx-jms-db/src/main/java/com/spring/dtx/tran7springdtxjmsdb/Tran7SpringDtxJmsDbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dtx.tran7springdtxjmsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tran7SpringDtxJmsDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tran7SpringDtxJmsDbApplication.class, args);
+    }
+
+}
